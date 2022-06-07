70ed74729268ebd14d033faa8ae6007b756e14ff
yash harkawat <yashharkawat2000@gmail.com>
2022-06-07 18:52:24 +0530

    highlighted node

--- index.js
+++ index.js
@@ -1,40 +1,3 @@
-// const search=(text,pattern)=>{
-
-//     let s=pattern.toLowerCase()+'#'+text.toLowerCase();
-//     let i=0,j=-1;
-//     let kmp=[];
-//     kmp[0]=-1;
-//     while(i<s.length)
-//     {
-//       while(j!==-1&&(s[i]!==s[j]))
-//       {
-//         j=kmp[j];
-//       }
-//       j+=1;
-//       i+=1;
-//       kmp[i]=j;
-//     }
-//     let index=[];
-//     //kmp[k-1]
-//     for(let k=0;k<kmp.length;k++)
-//     {
-//       //console.log(k,"--",s[k],"-",kmp[k]);
-//       if(kmp[k+1]===pattern.length)
-//       {
-//         index.push(k-2*pattern.length);
-//       }
-//     }
-//     return index;
-// }
-// let hasSubString = (str, sub) => {
-//     //For case sensitive
-//     //let re = new RegExp(sub,"g");
-  
-//     //for case insensitive
-//     let re = new RegExp(sub,"gi");
-  
-//     return (str.match(re) || []).length !== 0;
-//   }
 const dfs = (node,arrayOfNodes) => {
   if(node===null) return;
   arrayOfNodes.push(node);
@@ -45,7 +8,7 @@ const dfs = (node,arrayOfNodes) => {
 };
 const findNode=(pattern)=>{
   const array=[];
-  const arrayOfNodes=dfs(document,[]);
+  const arrayOfNodes=dfs(document.body,[]);
   arrayOfNodes.forEach((item)=>{
     if(item.nodeType===Node.TEXT_NODE)
     {
@@ -53,8 +16,18 @@ const findNode=(pattern)=>{
       if(text.indexOf(pattern)!=-1)
       {
         array.push(item);
+        //console.log(item.innerHTML);
+        let newElement=document.createElement("span");
+        newElement.classList.add("highLight");
+        newElement.innerHTML=item.textContent;
+        const par=item.parentNode;
+        par.replaceChild(newElement,item);
+        
       }
     }
+  })
+  document.querySelectorAll(".highLight").forEach((item)=>{
+    item.style.backgroundColor='Yellow';
   })
   return array;
 } 
